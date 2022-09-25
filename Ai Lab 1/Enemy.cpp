208311af9cbba5eaced581381aafb2cc15366a77
AedenMoylan <aedenmoylan@gmail.com>
2022-09-25 20:33:51 +0100

    added enemy that moves towards a random point and rotates towards it

--- Ai Lab 1/Enemy.cpp
+++ Ai Lab 1/Enemy.cpp
@@ -11,23 +11,54 @@ void Enemy::init()
 	enemySprite.setOrigin(enemySprite.getGlobalBounds().width / 2, enemySprite.getGlobalBounds().height / 2);
 	enemySprite.setScale(0.5, 0.5);
 	enemySprite.setPosition(500, 500);
-	enemySprite.rotate(180);
+	randPosX = (rand() % 1000) + 1;
+	randPosY = (rand() % 1000) + 1;
+
+	destinationCircle.setFillColor(sf::Color::White);
+	destinationCircle.setRadius(3);
+	destinationCircle.setOrigin(1.5, 1.5);
+	//(rand() % sf::VideoMode::getDesktopMode().width) + 1
 }
 
 void Enemy::draw(sf::RenderWindow& window)
 {
 	window.draw(enemySprite);
+	window.draw(destinationCircle);
 }
 
 void Enemy::update()
 {
 	movement();
+	destinationCircle.setPosition(randPosX, randPosY);
+	rotationCalculator();		
 }
 
 void Enemy::movement()
 {
-	enemySprite.move(speed, 0);
-	float whatever = enemySprite.getRotation();
+	velocity = { randPosX - enemySprite.getPosition().x, randPosY - enemySprite.getPosition().y };
+	float magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y));
+	velocity = { velocity.x / magnitude, velocity.y / magnitude };
+
+	enemySprite.move(velocity);
+
+	if (enemySprite.getGlobalBounds().intersects(destinationCircle.getGlobalBounds()))
+	{
+		randomizeWanderLocation();
+	}
 }
 
+void Enemy::randomizeWanderLocation()
+{
+	randPosX = (rand() % sf::VideoMode::getDesktopMode().width) + 1;
+	randPosY = (rand() % sf::VideoMode::getDesktopMode().height) + 1;
+}
 
+void Enemy::rotationCalculator()
+{
+	float dx = enemySprite.getPosition().x - randPosX;
+	float dy = enemySprite.getPosition().y - randPosY;
+
+	float rotation = (atan2(dy, dx)) * 180 / 3.14;
+
+	enemySprite.setRotation(rotation - 90);
+}
--- Ai Lab 1/Enemy.h
+++ Ai Lab 1/Enemy.h
@@ -2,6 +2,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
 
 class Enemy
 {
@@ -11,12 +14,19 @@ public:
 	void draw(sf::RenderWindow& window);
 	void update();
 	void movement();
+	void randomizeWanderLocation();
+	void rotationCalculator();
 
 private:
 	sf::Texture enemyTexture;
 	sf::Sprite enemySprite;
+	sf::CircleShape destinationCircle;
 	int speed = 3;
 	float rotation = 0;
+	sf::Vector2f randPos = { 0.0f,0.0f };
+	sf::Vector2f velocity = { 0.0f,0.0f };
+	float randPosX;
+	float randPosY;
 };
 
 
